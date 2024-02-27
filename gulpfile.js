764a2ed73c6fac034e04d8b394232170c63917f2
Liangjun Jiang <liajiang@expediagroup.com>
2024-02-26 21:31:03 -0600

    remove stylelint

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,6 @@ const cssnano = require('cssnano');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 
-var stylelint = require('gulp-stylelint');
-
-gulp.task('lint:css', function() {
-  return gulp
-    .src('./assets/*.css')
-    .pipe(stylelint({
-      reporters: [
-        {formatter: 'string', console: true}
-      ]
-    }));
-});
-
 gulp.task('styles', function () {
   return gulp
     .src('./assets/*.css')
